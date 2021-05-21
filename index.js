2aea43f89b6a4efa27a5df2bedfae18bece6e7bb
Tuan Pham <neves.pham@gmail.com>
2021-05-21 14:39:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 4000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from AWS App Runner!\n'
+  const msg = 'Hello from AWS App Runner HIIIII!\n'
   res.end(msg);
 });
 
